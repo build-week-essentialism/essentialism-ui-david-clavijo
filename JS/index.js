d90bf2c13ab018dd6047268c597aefd6aa34a54f
davichodavid <davichodavid@yahoo.com>
2019-04-16 17:07:20 -0400

    was able to figure out tab switcher, will now work on adding borders and text content

--- JS/index.js
+++ JS/index.js
@@ -1,38 +1,31 @@
-// const test = document.querySelectorAll(".signButton");
-// test.addEventListener("click", () => console.log("hey js is working"));
+class ButtonLink {
+  constructor(buttonElement) {
+    this.buttonElement = buttonElement;
+    this.tabData = this.buttonElement.dataset.tab;
+    this.cards = document.querySelectorAll(`.image[data-tab="${this.tabData}"]`);
+    this.cards = Array.from(this.cards).map(cardElement => new ButtonPicture(cardElement));
+    this.buttonElement.addEventListener("click", () => this.selectButton());
+  }
 
-const buttonse = document.querySelector("#signButton");
-console.log(buttonse);
+  selectButton() {
+    const buttons = document.querySelectorAll(".carouselButton");
+    buttons.forEach(button => button.classList.remove("buttonActive"));
+    const cards = document.querySelectorAll(".image");
+    cards.forEach(card => card.style.display = "none");
+    this.buttonElement.classList.add("buttonActive");
+    this.cards.forEach(card => card.selectPicture());
+  }
+}
 
+class ButtonPicture {
+  constructor(cardElement) {
+    this.cardElement = cardElement;
+  }
 
-// class ButtonLink {
-//   constructor(buttonElement) {
-//     this.buttonElement = buttonElement;
-//     this.dataButton = this.buttonElement.dataset.pic;
-//     this.pictures = document.querySelectorAll(`.carousel[data-pic="${dataButton}"]`);
-//     this.pictures.forEach(pictureElement => new ButtonPicture(pictureElement));
-//     this.buttonElement.addEventListener("click", () => selectButton());
-//   }
+  selectPicture() {
+    this.cardElement.style.display = "flex";
+  }
+}
 
-//   selectButton() {
-//     const buttons = document.querySelectorAll(".carouselButton");
-//     buttons.forEach(tab => tab.classList.remove("active-button"));
-//     const pictures = document.querySelectorAll(".carousel img");
-//     pictures.forEach(picture => picture.style.display = "none");
-//     this.buttonElement.classList.add("active-button");
-//     this.pictures.forEach(picture => picture.selectPicture());
-//   }
-// }
-
-// class ButtonPicture {
-//   constructor(pictureElement) {
-//     this.pictureElement = pictureElement;
-//   }
-
-//   selectPicture() {
-//     this.pictureElement.style.display = "flex";
-//   }
-// }
-
-// const buttons = document.querySelectorAll(".carouselButton");
-// buttons.forEach(buttonElement => new ButtonLink(buttonElement));
+const buttons = document.querySelectorAll(".carouselButton");
+buttons.forEach(buttonElement => new ButtonLink(buttonElement));
