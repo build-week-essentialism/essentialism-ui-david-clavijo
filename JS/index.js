a416e45f400a0470ea38f4f4677671d9e0b279f7
davichodavid <davichodavid@yahoo.com>
2019-04-16 13:42:10 -0400

    problem with JS carousel

--- JS/index.js
+++ JS/index.js
@@ -1,24 +1,38 @@
-class ButtonLink {
-  constructor(buttonElement) {
-    this.buttonElement = buttonElement;
-    this.dataButton = this.buttonElement.dataset.pic;
-    this.pictures = document.querySelectorAll(`.carousel[data-pic="${dataButton}"]`);
-    this.pictures.forEach(pictureElement => new SelectPicture(pictureElement));
-    this.buttonElement.addEventListener("click", () => selectButton());
-  }
+// const test = document.querySelectorAll(".signButton");
+// test.addEventListener("click", () => console.log("hey js is working"));
 
-  selectButton() {
-    const buttons = document.querySelectorAll(".carouselButton");
-    buttons.forEach(tab => tab.classList.remove("active-button"));
-    const pictures = document.querySelectorAll(".carousel img");
-  }
-}
+const buttonse = document.querySelector("#signButton");
+console.log(buttonse);
 
-class SelectPicture {
-  constructor(pictureElement) {
 
-  }
-}
+// class ButtonLink {
+//   constructor(buttonElement) {
+//     this.buttonElement = buttonElement;
+//     this.dataButton = this.buttonElement.dataset.pic;
+//     this.pictures = document.querySelectorAll(`.carousel[data-pic="${dataButton}"]`);
+//     this.pictures.forEach(pictureElement => new ButtonPicture(pictureElement));
+//     this.buttonElement.addEventListener("click", () => selectButton());
+//   }
 
-const buttons = document.querySelectorAll(".carouselButton");
-buttons.forEach(buttonElement => new ButtonLink(buttonElement));
+//   selectButton() {
+//     const buttons = document.querySelectorAll(".carouselButton");
+//     buttons.forEach(tab => tab.classList.remove("active-button"));
+//     const pictures = document.querySelectorAll(".carousel img");
+//     pictures.forEach(picture => picture.style.display = "none");
+//     this.buttonElement.classList.add("active-button");
+//     this.pictures.forEach(picture => picture.selectPicture());
+//   }
+// }
+
+// class ButtonPicture {
+//   constructor(pictureElement) {
+//     this.pictureElement = pictureElement;
+//   }
+
+//   selectPicture() {
+//     this.pictureElement.style.display = "flex";
+//   }
+// }
+
+// const buttons = document.querySelectorAll(".carouselButton");
+// buttons.forEach(buttonElement => new ButtonLink(buttonElement));
